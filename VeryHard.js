380168eb06dab514b305d2f2413077ccb9aa3d7b
Jose Eduardo Martinez <joseeduardomartinez@MacBook-Pro-3.local>
2022-05-12 08:57:57 -0400

    JS VeryHard

--- VeryHard.js
+++ VeryHard.js
@@ -11,24 +11,6 @@ function coins(coins, amount)
         table[i] = Number.MAX_VALUE;
     }
 
-    for (let i = 1; i <= amount; i++)
-    {
-      for (let j = 0; j < coins.length; j++){
-        if (coins[j] <= i)
-        {
-            let subResult = table[i - coins[j]];
-            if (subResult != Number.MAX_VALUE && subResult + 1 < table[i]){
-                table[i] = subResult + 1;  
-            }
-        } 
-      }  
-    }
-    
-    if(table[amount] == Number.MAX_VALUE){
-        return -1;
-    }
-    
-    return table[amount];
 }
 
 console.log(coins([4,5,6,7,0,1,2], 3));
