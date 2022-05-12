c82d1a9ecdee159be639174c85767898e4b522a9
Jose Eduardo Martinez <joseeduardomartinez@MacBook-Pro-3.local>
2022-05-11 22:26:08 -0400

    added js exercise VeryHard

--- VeryHard.js
+++ VeryHard.js
@@ -1,15 +1,34 @@
-
-function coins(coins, amount) {
-
-    let table = [amount+1];
-    for(let i = 0; i < amount + 1; i++) { //account for if the amount is 0 
+function coins(coins, amount)
+{
+    let table = new Array(amount+1);
+    for(let i = 0; i < amount + 1; i++) //account for if the amount is 0
+    {
         table[i] = 0;
     }
      
-    for (let i = 1; i <= amount; i++) { //initialize elements to max value representable in JS
+    for (let i = 1; i <= amount; i++) //initialize elements to max value
+    {
         table[i] = Number.MAX_VALUE;
     }
 
+    for (let i = 1; i <= amount; i++)
+    {
+      for (let j = 0; j < coins.length; j++){
+        if (coins[j] <= i)
+        {
+            let subResult = table[i - coins[j]];
+            if (subResult != Number.MAX_VALUE && subResult + 1 < table[i]){
+                table[i] = subResult + 1;  
+            }
+        } 
+      }  
+    }
+    
+    if(table[amount] == Number.MAX_VALUE){
+        return -1;
+    }
+    
+    return table[amount];
 }
 
-//This is as far as I could go :( 
+console.log(coins([4,5,6,7,0,1,2], 3));
